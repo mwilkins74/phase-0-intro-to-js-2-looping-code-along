a645dee855f24d6029c61804b866363e2c13b7de
Mark <markwilkins86@gmail.com>
2022-05-02 11:13:53 -0400

    commit tree 05d4d4948f7e9a5cd009ebef32ccf7ebc15cecd8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cards) {
+    let thanks = []
+    for (let i = 0; i < cards.length; i++)
+    thanks.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    return thanks
+}
+writeCards()
+
+function countDown(start) {
+    let i = start
+    while (i >= 0) {
+    console.log(i)
+    i--
+}}
